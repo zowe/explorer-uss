e688305e7e9664485d2aae527af27b78dc783919
Andrea Tabone <andrea.tabone@broadcom.com>
2026-08-11 15:01:29 +0200

    modify index.html
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

---
+++ WebContent/zlux-hooks/exports-shim.js
@@ -0,0 +1,16 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
+// ZoweZLUX_tempExports is a global contract expected by the zlux iframe-adapter script loaded after this file.
+/* eslint-disable camelcase */
+if (window.exports) {
+    window.ZoweZLUX_tempExports = window.exports;
+}
+window.exports = { __esModule: true };
